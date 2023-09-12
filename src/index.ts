80af762d053eabe209849fd9377a13b471f343ce
Wilarish <tararammmm2004@gmail.com>
2023-09-12 16:00:26 +0400

    commit init

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, {Request, Response} from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Helloh World!!!!!!')
+})
+app.get('/what', (req: Request, res: Response) => {
+    res.send('what?')
+})
+app.get('/videos', (req: Request, res: Response) => {
+    res.send('more videos')
+})
+app.get('/videos/:id', (req: Request, res: Response) => {
+    res.send('video number....')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
